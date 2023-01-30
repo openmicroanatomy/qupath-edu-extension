76e47436e0263440583f623037e1ce65394d6f47
Aaron Yli-Hallila <aaron.yli-hallila@student.oulu.fi>
2023-01-30 16:02:17 +0200

    fix: give warning for editing Lesson Information in a non-EduProject

--- src/main/java/qupath/edu/gui/dialogs/ProjectDescriptionEditorCommand.java
+++ src/main/java/qupath/edu/gui/dialogs/ProjectDescriptionEditorCommand.java
@@ -6,6 +6,7 @@ import qupath.edu.EduExtension;
 import qupath.edu.gui.CustomDialogs;
 import qupath.edu.EduProject;
 import qupath.lib.gui.QuPathGUI;
+import qupath.lib.gui.dialogs.Dialogs;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -16,19 +17,26 @@ public class ProjectDescriptionEditorCommand {
     private static final Logger logger = LoggerFactory.getLogger(ProjectDescriptionEditorCommand.class);
 
     public static void openDescriptionEditor() {
-        if (qupath.getProject() instanceof EduProject project) {
-            String initialInput = project.getProjectInformation();
-            Optional<String> result = CustomDialogs.showWYSIWYGEditor(initialInput);
+        if (!(qupath.getProject() instanceof EduProject project)) {
+            Dialogs.showErrorNotification(
+                "Error",
+                "The current project is not an EduProject -- cannot edit information."
+            );
 
-            if (result.isPresent()) {
-                project.setProjectInformation(result.get());
-                EduExtension.setProjectInformation(result.get());
+            return;
+        }
 
-                try {
-                    project.syncChanges();
-                } catch (IOException e) {
-                    logger.error("Error while syncing changes.");
-                }
+        String initialInput = project.getProjectInformation();
+        Optional<String> result = CustomDialogs.showWYSIWYGEditor(initialInput);
+
+        if (result.isPresent()) {
+            project.setProjectInformation(result.get());
+            EduExtension.setProjectInformation(result.get());
+
+            try {
+                project.syncChanges();
+            } catch (IOException e) {
+                logger.error("Error while syncing changes.");
             }
         }
     }
